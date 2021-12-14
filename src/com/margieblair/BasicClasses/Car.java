fd91c69c7f1372c43cd106a87ef79c4216f2693e
Marguerite Blair <marguerite.blair@gmail.com>
2021-12-14 14:18:11 -0500

    adding folder for console games

--- src/com/margieblair/BasicClasses/Car.java
+++ src/com/margieblair/BasicClasses/Car.java
@@ -2,11 +2,37 @@ package com.margieblair.BasicClasses;
 
 public class Car {
     private String licensePlate;
-    private String vin;
+    private String make;
+    private String model;
 
-    public Car(String licensePlate, String vin) {
+    public Car(String licensePlate, String make, String model) {
         this.licensePlate = licensePlate;
-        this.vin = vin;
+        this.make = make;
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
     }
 
     //need a getter to get a car by a specific identifier
--- src/com/margieblair/ConsoleGames/Main.java
+++ src/com/margieblair/ConsoleGames/Main.java
@@ -1,7 +1,23 @@
 package com.margieblair.ConsoleGames;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        while (true) {
+            int nextNum = scanner.nextInt();
+            try {
+                System.out.println("Input a new number:");
+                if (nextNum == 100) {
+                    System.out.println("Program ending...user input 100");
+                    break;
+                }
+            } catch (Exception e) {
+                System.out.println("You can only input a number within Java's bounds of an integer");
+                nextNum = -1;
+            }
+
+        }
     }
 }
