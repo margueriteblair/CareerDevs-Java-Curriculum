d83f8f58c763a2451fefcbecfe4dbbfc268f71dd
Marguerite Blair <marguerite.blair@gmail.com>
2021-12-14 16:49:57 -0500

    added cars to scenario

--- src/com/margieblair/BasicClasses/Main.java
+++ src/com/margieblair/BasicClasses/Main.java
@@ -3,15 +3,21 @@ package com.margieblair.BasicClasses;
 import java.util.*;
 
 public class Main {
-
+    static List<Car> availableCars = new ArrayList<>();
+    static List<Car> bookedCars = new ArrayList<>();
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        //make an available cars arraylist
-        //make a booked cars arraylist
-        //create a method that will take a param of a car's id/license/whatever and move it to the booked cars arrList
-        //call it bookCar(String id)
-        //create a similar method that will return the car - call it "returnCar(String id)"
+        availableCars.add(new Car("QWERTY", "Toyota", "Carola", 2010));
+        availableCars.add(new Car("HELLOWRLD", "Ford", "Thunderbird", 1990));
+        availableCars.add(new Car("JAVA", "Ford", "ModelT", 1920));
+        availableCars.add(new Car("MREEEH", "Honda", "Civic", 2012));
 
-        System.out.println("Changes");
+        System.out.println("Welcome to the car dealership! Please select which car you'd like to rent based on the license plate:");
+        for (int i = 0; i < availableCars.size(); i++) {
+            System.out.println(availableCars.get(i).toString());
+        }
+        String licenseSelected = scanner.next();
+
 
     }
 }
