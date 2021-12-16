894984df5c055369abf47cfa10b424966e00c803
Marguerite Blair <marguerite.blair@gmail.com>
2021-12-16 08:11:50 -0500

    breaking things up into a week by week package for the curriculum

---
+++ src/com/margieblair/Week01/GetFirstElement.java
@@ -0,0 +1,11 @@
+package com.margieblair.Week01;
+
+public class GetFirstElement {
+    public static void main(String[] args) {
+        getFirstElement(new int[] {5,6,1,2,12,13,44});
+    }
+
+    public static int getFirstElement(int[] arr) {
+        return arr[0];
+    }
+}
