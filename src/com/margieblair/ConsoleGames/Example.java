4a508294a873c612b883f4aa8cb2a9d8ba527381
Marguerite Blair <marguerite.blair@gmail.com>
2022-01-13 14:17:13 -0500

    pushing changes from today's lesson

---
+++ src/com/margieblair/ConsoleGames/Example.java
@@ -0,0 +1,5 @@
+package com.margieblair.ConsoleGames;
+
+public class Example {
+
+}
--- src/com/margieblair/ConsoleGames/Main.java
+++ src/com/margieblair/ConsoleGames/Main.java
@@ -1,23 +1,36 @@
 package com.margieblair.ConsoleGames;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
+    //static, belongs to the class
+    //non-static, belongs to instance
     public static void main(String[] args) {
-        while (true) {
-            int nextNum = scanner.nextInt();
-            try {
-                System.out.println("Input a new number:");
-                if (nextNum == 100) {
-                    System.out.println("Program ending...user input 100");
-                    break;
-                }
-            } catch (Exception e) {
-                System.out.println("You can only input a number within Java's bounds of an integer");
-                nextNum = -1;
-            }
+        System.out.println("Welcome to the program!");
+//        String firstName = firstName(); //"My name is Margie"; scanner.nextLine();
+//        System.out.println("Thank you! Hello, " + firstName);
+//        System.out.println("Now, please enter your last name: ");
+//        String lastName = scanner.next();
+//        System.out.println("Full name is : " + firstName + " " + lastName);
+//        System.out.println("Input a sentence: ");
+//        String sentence = scanner.nextLine(); //this will return a whole sentence
+//        System.out.println("You've input: " + sentence);
 
+        int sum = 0;
+        while (true) {
+            //prompt user to either play or quit
+            //make random here
+            //sum += randomNum
+            //print out a little summary of how many points/sum the user has
         }
+
+
+
+    }
+
+    public static String firstName() {
+        System.out.println("Please enter your first name: ");
+        return scanner.next();
     }
 }
