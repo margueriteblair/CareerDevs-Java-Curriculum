ebb1039d45d37bfda8b562d3c0a91c6801094c65
Marguerite Bliar <marguerite.blair@gmail.com>
2022-02-08 12:14:35 -0500

    shuffle method

--- src/com/margieblair/ConsoleGames/Blackjack/Deck.java
+++ src/com/margieblair/ConsoleGames/Blackjack/Deck.java
@@ -1,20 +1,32 @@
 package com.margieblair.ConsoleGames.Blackjack;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class Deck {
-    private List<Card> deck = new ArrayList<>();
+    private List<Card> deck;
+    private final static int[] RANKS = {1,2,3,4,5,6,7,8,9,10,11,12,13};
 
     public Deck() {
-        for (int i = 1; i <= 13; i++) {
+        this.deck = new ArrayList<>();
+        for (int i = 0; i < RANKS.length; i++) { //all of our ranks range from 1 - 13
             for (Suit s : Suit.values()) {
                 // {KEY:VALUE, KEY2, VAL2...}
                 // DO YOU WANT TO ITERATE OVER A) THE KEYS, B) THE VALUES, C) BOTH
-                deck.add(new Card(s, i));
+                deck.add(new Card(s, RANKS[i]));
                 System.out.println(deck.get(deck.size()-1));
             }
         }
+    }
+
+    public void shuffle() {
+        Collections.shuffle(deck);
+    }
+
+    public List<Card> getCards() {
+        return deck;
     }
     // List<Card> arrList = new ArrayList<>();
     // ArrayList<Card> arrList = new ArrayList<>();
