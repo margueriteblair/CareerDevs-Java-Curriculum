f29e780baae24c5b1d748a755cb107bd8cfd401f
Marguerite Bliar <marguerite.blair@gmail.com>
2022-02-18 21:24:02 -0500

    while true

--- src/com/margieblair/ConsoleGames/Hangman/Hangman.java
+++ src/com/margieblair/ConsoleGames/Hangman/Hangman.java
@@ -2,15 +2,52 @@ package com.margieblair.ConsoleGames.Hangman;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Hangman {
     public static void main(String[] args) throws FileNotFoundException {
 
         Scanner scanner = new Scanner(new File("/Users/margueriteblair/Downloads/words_alpha.txt"));
-
+        Scanner keyboard = new Scanner(System.in);
+        List<String> words = new ArrayList<>();
         while (scanner.hasNext()) {
-            System.out.println(scanner.nextLine());
+            words.add(scanner.nextLine());
         }
+
+        Random rand = new Random();
+        String word = words.get(rand.nextInt(words.size()));
+
+        System.out.println(word);
+
+        List<Character> playerGuesses = new ArrayList<>();
+
+        printWordState(word, playerGuesses);
+
+        while(true) {
+            getPlayerGuess(keyboard, word, playerGuesses);
+        }
+
+    }
+
+    static public void printWordState(String word, List<Character> playerGuesses) {
+        for (int i = 0; i < word.length(); i++) {
+            if (playerGuesses.contains(word.charAt(i))) {
+                System.out.print(word.charAt(i));
+            } else {
+                System.out.print("-");
+            }
+        }
+        System.out.println();
+    }
+
+    static public void getPlayerGuess(Scanner keyboard, String word, List<Character> playerGuesses) {
+        System.out.println("Please enter a letter:");
+        String letterGuess = keyboard.nextLine();
+        playerGuesses.add(letterGuess.charAt(0));
+
+        printWordState(word, playerGuesses);
     }
 }
